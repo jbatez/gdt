02ae5519feed1f95f2cbefdc43bdc999aa119a5b
Jo Bates <jbatez@protonmail.com>
2021-07-11 09:56:14 -0500

    Add license

--- test/panic.cxx
+++ test/panic.cxx
@@ -1,3 +1,8 @@
+// Copyright Jo Bates 2021.
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at:
+// https://www.boost.org/LICENSE_1_0.txt
+
 #include <gdt/panic.hxx>
 
 #include <cstdio>
